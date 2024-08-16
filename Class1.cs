5d18667b1738d697d6045af56256b68ff32b67e9
pjs70776 <patrick.j.stover.1985@gmail.com>
2024-08-16 08:54:13 -0700

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmissionAssignment
+{
+    class Class1
+    {
+        //method1 takes in two integers
+        public int method1(int x)
+        {
+            int Add = x + 10; return Add;
+            //int Add2 = z; return Add2;
+
+        }
+        public int method2(int z)
+        {
+            int Add2 = z + 0; return Add2;
+        }
+    }
+}
+
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 c1 = new Class1();
+
+            //method 1, 2 inputs
+            Console.WriteLine("Enter first mandatory integer here:");
+            int additon = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Enter second mandatory integer here:");
+            int secondInteger = Convert.ToInt32(Console.ReadLine());
+
+            //try catch statement
+            try
+            {
+                int additon2 = Convert.ToInt32(Console.ReadLine());
+                int sum2 = c1.method1(additon);
+                Console.WriteLine(sum2 + " is the answer.");
+            }
+            catch (Exception ex)
+            {
+                int additon2 = Convert.ToInt32(Console.ReadLine());
+                int sum2 = c1.method1(additon);
+                int sum3 = c1.method2(secondInteger);
+                Console.WriteLine(sum2 + " is the answer. And " + sum3 + " is the second integer.");
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
